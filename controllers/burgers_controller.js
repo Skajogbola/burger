f02baa770907f0fe94e106f94f83c00e947982ea
Simmie <skajogbola@users.noreply.github.com>
2019-05-13 23:50:27 -0400

    commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+// Import the model
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up the logic.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name"], [
+    req.body.burger_name], function(result) {
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
