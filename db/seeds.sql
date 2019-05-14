89f7238a5372a66c190cb7a172709046665cd5bf
Simmie <skajogbola@users.noreply.github.com>
2019-05-14 10:23:44 -0400

    commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Black Bean Burger", true),
        ("Bison Burger", true),
-       ("Veggie Burger", true);
+       ("Veggie Burger", true),
+       ("Turkey Burger", false),
+       ("Cheeseburger", false);
